136b3f191de027a453afaa1eb9c9f54e66154f4f
edensalt <124738007+edensalt@users.noreply.github.com>
2024-01-08 13:41:44 -0600

    Logo

--- app/ui/dashboard/sidenav.tsx
+++ app/ui/dashboard/sidenav.tsx
@@ -1,18 +1,23 @@
-import Link from 'next/link';
-import NavLinks from './nav-links';
-import { PowerIcon } from '@heroicons/react/24/outline';
-import AuthButtonServer from '@/app/auth-button-server';
-
+import Link from "next/link";
+import NavLinks from "./nav-links";
+import { PowerIcon } from "@heroicons/react/24/outline";
+import AuthButtonServer from "@/app/auth-button-server";
+import Image from "next/image";
 
 export default function SideNav() {
   return (
     <div className="flex h-full flex-col px-3 py-4 md:px-2">
       <Link
-        className="mb-2 flex h-20 items-end justify-start rounded-md bg-blue-600 p-4 md:h-40"
+        className="mb-2 flex h-20 items-end justify-start rounded-md bg-[#1060e4] p-4 md:h-40"
         href="/"
       >
-        <div className="w-32 text-white md:w-40">
-          LOGO
+        <div className="w-full h-16 flex items-end text-white relative">
+          <Image
+            src="/logo_transparent.png"
+            alt="Thanks Aunt Jill logo"
+            width={208}
+            height={24}
+          />
         </div>
       </Link>
       <div className="flex grow flex-row justify-between space-x-2 md:flex-col md:space-x-0 md:space-y-2">
